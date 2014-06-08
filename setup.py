401f36cac32273a5920d50f45961372b5057b67d
renyuanLyu <renyuan.lyu@gmail.com>
2014-06-08 20:21:12 +0800

    .
    ..

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 ﻿from distutils.core import setup
-setup(name='turtle_tc',
-      version='1.0',
-      py_modules=['turtle_tc'],
+
+setup(
+        name=         'turtle_tc',
+
+        py_modules=   ['turtle_tc', 'turtle_docstringdict_tc', 'examples_tc.ryYinyang'],
+
+        version=      '0.1',
+        description=  '繁體中文龜 turtle for traditional Chinese）',
+
+        author=       'Renyuan Lyu, 呂仁園。',
+        author_email= 'renyuan.lyu@gmail.com',
+        url=          'http://google.com/+RenyuanLyu',
+        download_url= 'https://github.com/renyuanL/pythonTurtleInChinese',  
+      
       )
 
